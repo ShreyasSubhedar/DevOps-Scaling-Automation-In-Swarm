f05a314822fdae12690242fd6cd727c49715aa13
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-08-23 01:12:35 +0530

    Adding Service

--- script.py
+++ script.py
@@ -10,3 +10,13 @@ import docker
 # To talk to a Docker daemon, you first need to instantiate a client.
 client = docker.from_env()
 
+# leaving the machine if already into swarm cluster
+client.swarm.leave(force=True)
+
+# initialzing swarm
+token = client.swarm.init()
+
+# creating serice
+service_editor = client.services.create(image="shreyasxo/code-editor:PHP",
+                                        name="Editor",
+                                        hosts={"8080": "8080"})
